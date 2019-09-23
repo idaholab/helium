216e82a9a316b04c97c12eec814afd31d1901414
David Andrs <david.andrs@inl.gov>
2019-09-23 08:50:03 -0600

    Adding range of validity
    Closes #7

--- include/userobjects/HeliumSBTLFluidProperties.h
+++ include/userobjects/HeliumSBTLFluidProperties.h
@@ -11,6 +11,13 @@ InputParameters validParams<HeliumSBTLFluidProperties>();
 /**
  * Properties of helium according to Ortiz et al. computed with the SBTL method
  *
+ * Range of validity:
+ *   0.0005 MPa <= p <= 100 MPa
+ *   250 K <= T <= 1300 K
+ *
+ * In this domain, helium is always in a gaseous state.
+ *
+ *
  * The following are the units used in libSBTL:
  * - pressure:           MPa
  * - temperature:        K
