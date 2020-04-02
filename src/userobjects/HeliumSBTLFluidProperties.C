ce47a249015ede48413711943093927a9c43e36b
David Andrs <david.andrs@inl.gov>
2020-04-02 14:36:24 -0600

    Implemented temporary zero derivatives for mu_from_v_e
    Refs #8

--- src/userobjects/HeliumSBTLFluidProperties.C
+++ src/userobjects/HeliumSBTLFluidProperties.C
@@ -223,6 +223,16 @@ HeliumSBTLFluidProperties::mu_from_v_e(Real v, Real e) const
   return ETA_VU_HE(v, e * _to_kJ);
 }
 
+void
+HeliumSBTLFluidProperties::mu_from_v_e(
+    Real v, Real e, Real & mu, Real & dmu_dv, Real & dmu_de) const
+{
+  mu = mu_from_v_e(v, e);
+  // currenlty there is no API for derivatives of mu in SBTL package
+  dmu_dv = 0;
+  dmu_de = 0;
+}
+
 Real
 HeliumSBTLFluidProperties::k_from_v_e(Real v, Real e) const
 {
